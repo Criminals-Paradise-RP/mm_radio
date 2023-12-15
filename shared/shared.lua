3642b7839378d0ea9545cdf64547380a5f0d8195
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-12-14 20:51:02 -0500

    Update shared.lua

--- shared/shared.lua
+++ shared/shared.lua
@@ -3,43 +3,43 @@ Shared.MaxFrequency = 500.00 -- Max Limit of Radio Channel
 Shared.RestrictedChannels = {
     [1] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [2] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [3] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [4] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [5] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [6] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [7] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [8] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [9] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [10] = { -- channel id
         type = 'job', -- job/gang
-        name = {"police", "ambulance"}
+        name = {"police", "ambulance", "sasp", "sapr", "bcso"}
     },
     [420] = { -- channel id
         type = 'gang', -- job/gang
